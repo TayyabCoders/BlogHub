811915740099e9e5591f9dd6f0e027e304014693
Tayyab Faisal <codertayyab101@gmail.com>
2025-01-20 19:37:51 +0500

    adding files for deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1+*9^bw^ig_#3cuy##4m%gs-^znhc5ndltft38bn@8!@h8x0o1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
